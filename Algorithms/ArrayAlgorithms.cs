aa97b74690854b6b743c92c26d24123943fec39e
Nadxelle Hernandez <nadxelleHernandez@gmail.com>
2023-10-30 22:25:00 -0700

    Added stack petri problem

---
+++ Algorithms/ArrayAlgorithms.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+
+namespace Algorithms {
+    class ArrayAlgorithms {
+        public static int[] petriPropagation(int []petri) {
+            Stack <int> biggest = new Stack<int>();
+            int last = petri.Length -1;
+            biggest.Push(petri[last]);
+            petri[last] = -1;
+            for(int i=last-1; i>=0; i--) {
+                int top = biggest.Peek();
+                biggest.Push(petri[i]);
+                if(petri[i] < top) {
+                    petri[i] = top;
+                } else {
+                    while(biggest.Count>0 && petri[i] >= biggest.Peek()) {
+                        biggest.Pop();
+                    }
+                    if (biggest.Count == 0) {
+                        biggest.Push(petri[i]);
+                        petri[i] = -1;
+                    } else {
+                        top = biggest.Peek();
+                        biggest.Push(petri[i]);
+                        petri[i] =top;
+                    }
+                }
+            }
+
+            return petri;
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.InteropServices;
-using Algorithms;
+﻿using Algorithms;
 
 string input = "the sky is blue";
 
@@ -16,6 +15,16 @@ input = " this   is    a funky string";
 result = StringAlgorithms.ReverseWords(input);
 
 Console.WriteLine(result);
+
+Console.WriteLine("##Testing petri dish##");
+
+int [] petri = new int[] {1,5,2,6,1,3,4,6};
+
+int [] petriExpanded = ArrayAlgorithms.petriPropagation(petri);
+
+Array.ForEach(petriExpanded, Console.WriteLine);
+
+
 
 
 
